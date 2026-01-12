cc67f0b70a468c06957895cd0afcf96376b28baf
Callixte Muller <callixte.muller@proton.me>
2026-01-12 13:58:28 +0100

    utils.h

---
+++ utils.h
@@ -0,0 +1,12 @@
+//
+// Created by Callixte Muller on 12/01/2026.
+//
+
+#ifndef MINI_PROJET_UTILS_H
+#define MINI_PROJET_UTILS_H
+
+struct Picture {
+    int width, height;
+};
+
+#endif //MINI_PROJET_UTILS_H
